2d871ca7cdf2aae840eb1f0247f3a97339245b28
Juan Victor Aleixo <juanvaleixo@gmail.com>
2021-11-26 17:23:41 -0300

    parte 1
    adicionei as letras das questões

--- parte1.pl
+++ parte1.pl
@@ -1,10 +1,10 @@
 
-potencia(A,B,X) :-  X is A^B. %
+potencia(A,B,X) :-  X is A^B. %c
 
-acrescentar([],X,[X]).
+acrescentar([],X,[X]). %a
 acrescentar([Y|YS],X,[Y|K]) :- acrescentar(YS,X,K).
 
-inserir([],X,I,[X]).
+inserir([],X,I,[X]). %b
 inserir([Y|YS],X,1,[X,Y|YS]).
 inserir([Y|YS],X,I,[Y|K]) :-
     I2 is I-1,
